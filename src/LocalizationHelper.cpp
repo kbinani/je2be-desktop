3e937ee492222f8e9b68b533ffa2b645b0f28469
kbinani <kbinani.bt@gmail.com>
2022-12-14 19:53:25 +0900

    Replace GetSystemDefaultUILanguage to GetSystemPreferredUILanguages

--- src/LocalizationHelper.cpp
+++ src/LocalizationHelper.cpp
@@ -12,8 +12,21 @@ using namespace juce;
 namespace je2be::desktop {
 
 LocalisedStrings *LocalizationHelper::CurrentLocalisedStrings() {
-  LANGID lang = GetSystemDefaultUILanguage();
-  if (lang == 0x0411) {
+  using namespace std;
+  ULONG num = 0;
+  ULONG size = 0;
+  if (!GetSystemPreferredUILanguages(MUI_LANGUAGE_NAME, &num, nullptr, &size)) {
+    return nullptr;
+  }
+  wstring buffer;
+  buffer.resize(size, (WCHAR)0);
+  num = 0;
+  size = buffer.size();
+  if (!GetSystemPreferredUILanguages(MUI_LANGUAGE_NAME, &num, buffer.data(), &size)) {
+    return nullptr;
+  }
+  String preferred(buffer.c_str());
+  if (preferred == "ja-JP") {
     return Japanese();
   }
   return nullptr;
