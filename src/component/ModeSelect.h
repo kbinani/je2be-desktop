364167015d2494073469037a2e6046595784a257
kbinani <kbinani.bt@gmail.com>
2024-01-12 06:00:26 +0900

    Update mode select panel

--- src/component/ModeSelect.h
+++ src/component/ModeSelect.h
@@ -7,6 +7,17 @@ namespace je2be::desktop::component {
 class TextButton;
 
 class ModeSelect : public juce::Component {
+  enum class From {
+    Java,
+    Bedrock,
+    Xbox360,
+    PS3,
+  };
+  enum class To {
+    Java,
+    Bedrock,
+  };
+
 public:
   explicit ModeSelect();
   ~ModeSelect();
@@ -14,18 +25,37 @@ public:
   void paint(juce::Graphics &) override;
 
   void onAboutButtonClicked();
-  void onJavaToBedrockButtonClicked();
-  void onBedrockToJavaButtonClicked();
-  void onXbox360ToJavaButtonClicked();
-  void onXbox360ToBedrockButtonClicked();
+  void onNextButtonClicked();
+
+private:
+  void update();
 
 private:
   std::unique_ptr<juce::Label> fLabel;
-  std::unique_ptr<TextButton> fJavaToBedrockButton;
-  std::unique_ptr<TextButton> fBedrockToJavaButton;
-  std::unique_ptr<TextButton> fXbox360ToJavaButton;
-  std::unique_ptr<TextButton> fXbox360ToBedrockButton;
+  std::unique_ptr<TextButton> fFromJavaButton;
+  std::unique_ptr<TextButton> fFromBedrockButton;
+  std::unique_ptr<TextButton> fFromXbox360Button;
+  std::unique_ptr<TextButton> fFromPS3Button;
+  std::unique_ptr<TextButton> fToJavaButton;
+  std::unique_ptr<TextButton> fToBedrockButton;
   std::unique_ptr<TextButton> fAboutButton;
+  std::unique_ptr<TextButton> fNextButton;
+  juce::Path fJavaToBedrockPath;
+  juce::Path fBedrockToJavaPath;
+  juce::Path fXbox360ToJavaPath;
+  juce::Path fXbox360ToBedrockPath;
+  juce::Path fPS3ToJavaPath;
+  juce::Path fPS3ToBedrockPath;
+  juce::Path fArrowHeadBedrockToJava;
+  juce::Path fArrowHeadJavaToBedrock;
+  juce::Path fArrowHeadXbox360ToJava;
+  juce::Path fArrowHeadXbox360ToBedrock;
+  juce::Path fArrowHeadPS3ToJava;
+  juce::Path fArrowHeadPS3ToBedrock;
+  std::optional<From> fFrom;
+  std::optional<To> fTo;
+  std::optional<From> fLastFrom;
+  std::optional<To> fLastTo;
 
   JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(ModeSelect)
 };
